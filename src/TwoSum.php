c63b41a278b62693f05b167b4ecb0998bdcf5c87
Sergey Vetko <s.vetko@itgr.ru>
2022-02-03 18:29:21 +0300

    Improved TwoSum - using hash map!

--- src/TwoSum.php
+++ src/TwoSum.php
@@ -3,45 +3,36 @@
 namespace SV;
 
 /**
- * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-You can return the answer in any order.
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to
+ * target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * You can return the answer in any order.
  *
  * Learn a good solution here: https://www.code-recipe.com/post/two-sum
  */
 class TwoSum {
 
 	/**
-	 * Mine version. Too slow. It's n^3
-	 *
-	 * @param Integer[] $nums
-	 * @return array []
+	 * New version. Must be much faster
+	 * @param $nums
+	 * @param $target
+	 * @return array
 	 */
 	public function solve($nums, $target): array {
-		$foundKeys = [];
+		$keysMap = [];
 
-		$len = count($nums);
-
-		for ($i = 0; $i < $len; $i++) {
-			$num1 = $nums[$i];
-
-			for ($j = 0; $j < $len; $j++) {
-				$num2 = $nums[$j];
-				if ($i === $j) {
-					continue;
-				}
-
-				$sum = $num1 + $num2;
-				if ($sum === $target) {
-					$foundKeys = [$i, $j];
-					break 2;
-				}
+		foreach ($nums as $index => $num) {
+			$reqNumber = $target - $num;
+			if (array_key_exists($reqNumber, $keysMap)) {
+				return [$keysMap[$reqNumber], $index];
 			}
+
+			$keysMap[$num] = $index;
 		}
 
-		return $foundKeys;
+		return [];
 	}
 }
 
